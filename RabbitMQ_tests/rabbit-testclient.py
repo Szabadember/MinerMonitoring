708e2dbd1c625a1d5a940e95a7f6878cd6300353
Áron Szabados <aron.szabados@skyscanner.net>
2017-08-11 15:30:29 +0200

    Switched to topic exchange

--- RabbitMQ_tests/rabbit-testclient.py
+++ RabbitMQ_tests/rabbit-testclient.py
@@ -1,17 +1,20 @@
+#!/usr/bin/env python
 import pika
-import random
+import sys
+
+EXCHANGE_NAME = "monitoring_exchange"
 
 parameters = pika.URLParameters('amqp://minerrig1:Qgjk5234@localhost:5672/%2F')
-
 connection = pika.BlockingConnection(parameters)
-
 channel = connection.channel()
-channel.queue_declare(queue='minerrig1')
 
-channel.basic_publish('',
-                      'minerrig1',
-                      str(random.randint(1, 10)),
-                      pika.BasicProperties(content_type='text/plain',
-                                           delivery_mode=1))
+channel.exchange_declare(exchange=EXCHANGE_NAME,
+                         type='topic')
 
+routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
+message = ' '.join(sys.argv[2:]) or 'Hello World!'
+channel.basic_publish(exchange=EXCHANGE_NAME,
+                      routing_key=routing_key,
+                      body=message)
+print(" [x] Sent %r:%r" % (routing_key, message))
 connection.close()
--- RabbitMQ_tests/rabbit-testconsumer.py
+++ RabbitMQ_tests/rabbit-testconsumer.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 import pika
+import sys
+
+EXCHANGE_NAME = "monitoring_exchange"
 
 parameters = pika.URLParameters('amqp://minerrig1:Qgjk5234@localhost:5672/%2F')
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
-channel.queue_declare(queue='minerrig1')
+channel.exchange_declare(exchange=EXCHANGE_NAME,
+                         type='topic')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(exchange=EXCHANGE_NAME,
+                       queue=queue_name,
+                       routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    print(" [x] %r:%r" % (method.routing_key, body))
 
 channel.basic_consume(callback,
-                      queue='minerrig1',
+                      queue=queue_name,
                       no_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
